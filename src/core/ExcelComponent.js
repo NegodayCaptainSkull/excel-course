c2cb1317e47ce0acaec343691ed70363e15ee072
NegodayCaptainSkull <goshamkrtchian@gmail.com>
2022-05-01 15:11:32 +0400

    finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,18 +4,41 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscribers = []
+
+    this.prepare()
   }
+
+  // Настратваем наш компонент до init
+  prepare() {}
 
   // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
 
+  // уведомляем слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // подписываемся на событие ивент
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // Инициальзируем компонент
+  // добавляем дом слушателей
   init() {
     this.initDOMListeners()
   }
 
+  // Удаляем компонент
+  // Чистим слушателей
   destroy() {
     this.removeDOMlisteners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
